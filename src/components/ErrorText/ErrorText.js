119d130b9aeb924946c24bce647256d3fab34504
tugba-koc <koctug@itu.edu.tr>
2021-12-21 15:28:53 +0300

    if no result, show error text

--- src/components/ErrorText/ErrorText.js
+++ src/components/ErrorText/ErrorText.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-function ErrorText({ text }) {
+function ErrorText({ text }) { 
   return (
     <div>
       <div className="error-text">"{text}" için sonuç bulunmamaktadır.</div>
